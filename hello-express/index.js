41011b530628e98738a6162b62b29a82f943f4a1
Breno R Monteiro <monteiro1breno@gmail.com>
2016-11-29 11:09:49 -0300

    Anakin Version

--- hello-express/index.js
+++ hello-express/index.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 
 app.get("/hello", (req,res) => res.send("Olá mundo!"));
+
 app.get("/greet", (req,res) => res.send(`Olá, ${req.query.nome}!`));
+
+app.get("/queryparams", (req,res) => {
+  console.log(req.query);
+  res.send("Veja no console");
+});
 
 app.listen(3000);
