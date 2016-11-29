d83bb07ad0f17febc47235969060cce7d2b4fbc7
Breno R Monteiro <monteiro1breno@gmail.com>
2016-11-29 11:01:15 -0300

    Começando as Rotas

--- hello-express/index.js
+++ hello-express/index.js
@@ -3,5 +3,6 @@ const express = require("express");
 const app = express();
 
 app.get("/hello", (req,res) => res.send("Olá mundo!"));
+app.get("/greet", (req,res) => res.send(`Olá, ${req.query.nome}!`));
 
 app.listen(3000);
